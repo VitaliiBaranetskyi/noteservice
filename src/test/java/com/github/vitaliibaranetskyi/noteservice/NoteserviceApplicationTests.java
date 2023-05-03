30d5c7fcfe8d50efe793faee05de9ad9ba229aa6
Vitalii <vitalik.baranetskiy@gmail.com>
2023-05-03 19:40:35 +0300

    Initial commit

---
+++ src/test/java/com/github/vitaliibaranetskyi/noteservice/NoteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vitaliibaranetskyi.noteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
